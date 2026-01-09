2f3e7e814d96e135f2698d9115e3f6ed1cd2a1e0
Pome <unclepome.dev@gmail.com>
2026-01-09 14:45:44 +0900

    Enables suppression for Blender naming convention
    Extends the inspection suppressor to handle Blender's specific naming convention for classes.
    
    This prevents false positives for naming violations related to Blender's style, improving code analysis accuracy.

--- src/main/kotlin/com/github/unclepomedev/blenderprobeforpycharm/inspection/BlenderPropertySuppressor.kt
+++ src/main/kotlin/com/github/unclepomedev/blenderprobeforpycharm/inspection/BlenderPropertySuppressor.kt
@@ -6,6 +6,7 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.python.psi.PyAnnotation
 import com.jetbrains.python.psi.PyCallExpression
+import com.jetbrains.python.psi.PyClass
 import com.jetbrains.python.psi.PyQualifiedNameOwner
 import com.jetbrains.python.psi.PyReferenceExpression
 
@@ -18,11 +19,28 @@ class BlenderPropertySuppressor : InspectionSuppressor {
         "RemoveProperty"
     )
 
+    private val blenderNamingRegex = Regex("^[A-Z][A-Z0-9_]+_[A-Z]{2}_[a-z0-9_]+$")
+
+    @Suppress("UnstableApiUsage")
     override fun isSuppressedFor(element: PsiElement, toolId: String): Boolean {
-        val allowedIds = setOf("PyTypeChecker", "PyAnnotation", "PyTypeHints")
+        val allowedIds = setOf("PyTypeChecker", "PyAnnotation", "PyTypeHints", "PyPep8Naming")
+
         if (toolId !in allowedIds) {
             return false
         }
+
+        if (toolId == "PyPep8Naming") {
+            val pyClass = element as? PyClass ?: PsiTreeUtil.getParentOfType(element, PyClass::class.java)
+
+            if (pyClass != null) {
+                val className = pyClass.name
+                if (className != null && blenderNamingRegex.matches(className)) {
+                    return true
+                }
+            }
+        }
+
+        if (toolId == "PyPep8Naming") return false
 
         val annotation = PsiTreeUtil.getParentOfType(element, PyAnnotation::class.java) ?: return false
         val value = annotation.value as? PyCallExpression ?: return false
@@ -38,6 +56,7 @@ class BlenderPropertySuppressor : InspectionSuppressor {
             return false
         }
         val text = callee.text ?: return false
+
         @Suppress("UnstableApiUsage")
         val name = callee.referencedName ?: return false
 
