e8f6f3189521b2c6da85d9cdbe4a771034399325
Pome <unclepome.dev@gmail.com>
2026-01-13 04:03:37 +0900

    Adds icons and refactors icon loading
    Introduces dedicated icon files and a central icon registry.
    This streamlines icon management and allows for easier updates and theming.
    
    Replaces direct icon loading with references to the icon registry,
    improving code maintainability and reducing redundancy.

---
+++ src/main/kotlin/com/github/unclepomedev/blenderprobeforpycharm/icons/BlenderProbeIcons.kt
@@ -0,0 +1,9 @@
+package com.github.unclepomedev.blenderprobeforpycharm.icons
+
+import com.intellij.openapi.util.IconLoader
+
+object BlenderProbeIcons {
+    @JvmField
+    val Logo16 = IconLoader.getIcon("/icons/pluginIcon.svg", javaClass)
+    val Logo16_2 = IconLoader.getIcon("/icons/pluginIcon2.svg", javaClass)
+}
--- src/main/kotlin/com/github/unclepomedev/blenderprobeforpycharm/run/BlenderTestConfigurationType.kt
+++ src/main/kotlin/com/github/unclepomedev/blenderprobeforpycharm/run/BlenderTestConfigurationType.kt
@@ -1,15 +1,15 @@
 package com.github.unclepomedev.blenderprobeforpycharm.run
 
+import com.github.unclepomedev.blenderprobeforpycharm.icons.BlenderProbeIcons
 import com.intellij.execution.configurations.ConfigurationTypeBase
 import com.intellij.execution.configurations.ConfigurationTypeUtil
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.NotNullLazyValue
 
 class BlenderTestConfigurationType : ConfigurationTypeBase(
     ID,
     "Blender Test",
     "Run Blender tests",
-    NotNullLazyValue.createValue { AllIcons.Nodes.Test }
+    NotNullLazyValue.createValue { BlenderProbeIcons.Logo16_2 }
 ) {
     init {
         addFactory(BlenderTestConfigurationFactory(this))
